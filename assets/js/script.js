9c4d79b617a627ce1e917a2dff65a83b77511e67
Dahlia Guido <dguido.dev@gmail.com>
2023-07-14 00:29:18 -0500

    display current weather phase1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,52 @@
 // var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=7a2a3e009ff8ece7e90ad8dae53147aa"
 
 // *** Document References
-var searchFormEl = document.querySelector('#searchForm'); 
-var cityInputVal = document.querySelector('#searchCity');
+var searchFormEl = document.getElementById('searchForm'); 
+var cityInputVal = document.getElementById('searchCity');
 var searchBtn = document.querySelector ('.btn-primary');
-var currentCityInfo = document.querySelector('#currentCityInfo');
-var currentTemp = document.querySelector('#currentTemp');
-var currentWind = document.querySelector('#currentWind');
-var currentHumidity = document.querySelector('#currentHumidity');
-var rightNowTD = dayjs().format('dddd, MMMM D YYYY h:m a ').toString();
+var currentCityInfo = document.getElementById('currentCityInfo');
+var currentTemp = document.getElementById('currentTemp');
+var currentWind = document.getElementById('currentWind');
+var currentHumidity = document.getElementById('currentHumidity');
 
 
- 
-var displayForecast = {
+
+
+ // Fetch current weather from searched city
+var weather = {
   keyID : '7a2a3e009ff8ece7e90ad8dae53147aa',
-  fetchWeather: function (){
+  fetchWeather: function (city){
     fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=London&appid=7a2a3e009ff8ece7e90ad8dae53147aa'
+      'https://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=imperial&appid=' + this.keyID
     )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => this.displayWeather(data));
+  },
+  displayWeather : function(data){
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log (name, icon, description,temp,humidity, speed);
+    document.querySelector('#currentCityInfo').innerHTML = 'Weather in ' + name;
+    document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+    document.querySelector('#timeDate').innerHTML = dayjs().format('dddd, MMMM D YYYY h:m a').toString();
+    document.querySelector('.weatherDescription').innerHTML = description;
+    document.querySelector('.grados').innerHTML = temp + '°F';
+    document.querySelector('#currentHumidity').innerHTML = `Humidity
+     ${humidity}%` ;
+    document.querySelector('#currentWind').innerHTML = 'Wind speed ' + speed + 'miles/h';
+  },
+  search : function(){
+    this.fetchWeather(cityInputVal);
   }
-  
 
 
-}
+};
 
-//displayForecast()
 
-// console.log(currentWeather);
-console.log (rightNowTD);
+searchBtn.addEventListener('click', function(){
+  weather.search();
 
-// searchBtn.addEventListener('submit', displayForecast);
+});
 
